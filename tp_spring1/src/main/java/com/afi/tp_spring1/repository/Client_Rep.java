6015a1116cd1ec1fd446678d96d040094f1d2a08
dioufabo <layediouf96@gmail.com>
2025-08-05 20:35:44 +0000

    renomer Client_Rep

--- tp_spring1/src/main/java/com/afi/tp_spring1/repository/Client_Rep.java
+++ tp_spring1/src/main/java/com/afi/tp_spring1/repository/Client_Rep.java
@@ -3,10 +3,10 @@ package com.afi.tp_spring1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-import com.afi.tp_spring1.model.Etudiant;
+import com.afi.tp_spring1.model.Client;
 @Component
 
-public interface Client_Rep extends JpaRepository<Etudiant,Long> {
+public interface Client_Rep extends JpaRepository<Client,Long> {
 
 	
 	
